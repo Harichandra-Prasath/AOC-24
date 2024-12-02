4814bf4d2aeae8754e19abb644345c62c31b496f
Harichandra-Prasath <1hcp302004@gmail.com>
2024-12-02 13:34:37 +0530

    Day-2 P-1

---
+++ Calendar/Day-2/helper.go
@@ -0,0 +1,3 @@
+package day2
+
+const INPUT_FILE = `Calendar/Day-2/input.txt`
--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 
 	day1 "github.com/Harichandra-Prasath/AOC-24/Calendar/Day-1"
+	day2 "github.com/Harichandra-Prasath/AOC-24/Calendar/Day-2"
 )
 
 func main() {
 
 	fmt.Println("Solution for Day-1 Problem-1:", day1.P1_Solve())
-	fmt.Println("Solution for Day-2 Problem-2:", day1.P2_Solve())
+	fmt.Println("Solution for Day-1 Problem-2:", day1.P2_Solve())
+	fmt.Println("Solution for Day-2 Problem-1:", day2.P1_Solve())
+
 }
